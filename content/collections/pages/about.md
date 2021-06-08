4307c3c925a4aaff40afdbb221a2fa20cbee198a
Jamie Throup <jamiethroup@gmail.com>
2021-06-08 13:26:20 +0100

    Initial Commit

---
+++ content/collections/pages/about.md
@@ -0,0 +1,9 @@
+---
+id: 750505bf-094c-4332-acbe-34d45e448afa
+title: About
+author: cab3a84c-346e-4c9b-9d84-261bb9e35896
+template: default
+updated_by: cab3a84c-346e-4c9b-9d84-261bb9e35896
+updated_at: 1620655196
+---
+This is an example paragraph
---
+++ tailwind.config.js
@@ -0,0 +1,64 @@
+  module.exports = {
+    purge: {
+        content: [
+            './resources/**/*.antlers.html',
+            './resources/**/*.blade.php',
+            './content/**/*.md'
+        ]
+    },
+    theme: {
+        extend: {
+            flex: {
+                '2': '2 2 0%'
+            },
+            inset: {
+                '24': '6rem'
+            },
+            colors: {
+                primary: 'var(--color-primary)',
+                secondary: 'var(--color-secondary)',
+            },
+            width: {
+                '72': '18rem',
+                '84': '21rem',
+                '96': '24rem',
+                '108': '27rem',
+                '120': '30rem',
+                '132': '33rem',
+                '144': '36rem'
+            },
+            maxWidth: {
+                '1/4': '25%',
+                '1/2': '50%',
+                '3/4': '75%'
+            },
+            screens: {
+                'sm-max': {
+                    'max': '639px'
+                },
+                'md-max': {
+                    'max': '739px'
+                }
+            }
+        },
+        fontFamily: {
+            'sans': 'Montserrat, sans-serif',
+            'serif': 'Libre Baskerville, serif',
+            // 'headings': 'GTAmericaCompressedLight, sans-serif',
+            // 'headings-alt': 'LOUDCompressedLight, sans-serif'
+        },
+        fontSize: {
+            xs: ['1rem', '1.5'],
+            sm: ['1.2rem', '1.5'],
+            base: ['1.3rem', '1.5'],
+            lg: ['1.6rem', '1.5'],
+            xl: ['1.8rem', '1.5'],
+            '2xl': ['2rem', '1.5'],
+            '3xl': ['2.4rem', '1.5'],
+            '4xl': ['3rem', '1.5'], 
+            '5xl': ['3.6rem', '1.5']
+        }
+    },
+    variants: {},
+    plugins: []
+}
